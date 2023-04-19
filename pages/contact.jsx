022a9db052e4d7f8eb925858fd7e3fc1d88f033d
Demchenk0 <santadyr2016@gmail.com>
2023-04-19 18:03:02 +0300

    done

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Hero from './../components/Hero';
+import Contact from './../components/Contact';
 
 const contact = () => {
   return (
     <div>
-      contact
+      <Hero heading='Contact' message='Submit the form below for more work and quotes.' />
+      <Contact />
     </div>
   )
 }
